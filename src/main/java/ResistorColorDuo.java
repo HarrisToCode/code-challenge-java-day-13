c6a597d3677a4b8fbbace77c0d58a2f5b780560d
Harris <40709470+Ogbarrison@users.noreply.github.com>
2020-05-12 18:20:23 +0100

    resistor challenge 5 tests passed

--- src/main/java/ResistorColorDuo.java
+++ src/main/java/ResistorColorDuo.java
@@ -1,5 +1,42 @@
 class ResistorColorDuo {
     int value(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String resultString = "";
+        for (String color :
+                colors) {
+            switch (color) {
+                case "black":
+                    resultString += String.valueOf(0);
+                    break;
+                case "brown":
+                    resultString += String.valueOf(1);
+                    break;
+                case "red":
+                    resultString += String.valueOf(2);
+                    break;
+                case "orange":
+                    resultString += String.valueOf(3);
+                    break;
+                case "yellow":
+                    resultString += String.valueOf(4);
+                    break;
+                case "green":
+                    resultString += String.valueOf(5);
+                    break;
+                case "blue":
+                    resultString += String.valueOf(6);
+                    break;
+                case "violet":
+                    resultString += String.valueOf(7);
+                    break;
+                case "grey":
+                    resultString += String.valueOf(8);
+                    break;
+                case "white":
+                    resultString += String.valueOf(9);
+                    break;
+            }
+        }
+        return Integer.parseInt(resultString.substring(0,2));
+        //        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 }
